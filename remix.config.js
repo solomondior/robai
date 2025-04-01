3813c463332ac5ac7dacc80270f337100983585a
Zubair Hersi <hersizubair@gmail.com>
2025-04-01 19:42:43 +0100

    Fix build commands for Remix v1 compatibility with Vercel

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"]
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "cjs",
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "build",
+  devServerPort: 8002
 }; 
